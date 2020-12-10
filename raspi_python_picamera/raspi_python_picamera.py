6e26143ec8e9d71e578a12737fb26159f09160e8
coyote009 <33458465+coyote009@users.noreply.github.com>
2020-12-11 00:45:42 +0900

    RasPi camera capture sample in python: much simpler way

--- raspi_python_picamera/raspi_python_picamera.py
+++ raspi_python_picamera/raspi_python_picamera.py
@@ -11,26 +11,18 @@ import io
 import numpy as np
 import cv2
 import picamera
+import picamera.array
 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=30)
+buf = picamera.array.PiRGBArray(camera, size=(640, 480))
 
-#camera.start_preview()
+for frame in camera.capture_continuous(buf, format='bgr',
+                                       use_video_port=True):
 
-stream = io.BytesIO()
-for _ in camera.capture_continuous(stream, format='raw',
-                                   use_video_port=True):
-
-    buf = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-    buf = buf.reshape(-1, 640)
-
-    img = cv2.cvtColor(buf, cv2.COLOR_YUV2BGR_I420)
-
-    stream.seek(0)
-    stream.truncate()
-
-    cv2.imshow("img", img)
+    cv2.imshow("img", frame.array)
     if cv2.waitKey(1) == 27:
         break
 
-#camera.stop_preview()
+    buf.truncate(0)
+
 camera.close()
