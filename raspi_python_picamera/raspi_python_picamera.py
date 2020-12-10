fa26e0952602a96e6414a08a0ad0d6c21523caa5
coyote009 <33458465+coyote009@users.noreply.github.com>
2020-12-10 10:01:46 +0900

    RasPi camera capture sample in python

---
+++ raspi_python_picamera/raspi_python_picamera.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Capture image from RasPi Cam
+-Prerequisites
+ -Install OpenCV: pip install opencv-python
+ -picamera is already installed if raspbian
+"""
+
+import io
+import numpy as np
+import cv2
+import picamera
+
+with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera:
+    camera.start_preview()
+
+    stream = io.BytesIO()
+    for _ in camera.capture_continuous(stream, format='raw',
+                                       use_video_port=True):
+
+        buf = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+        buf = buf.reshape(-1, 640)
+
+        img = cv2.cvtColor(buf, cv2.COLOR_YUV2BGR_I420)
+
+        stream.seek(0)
+        stream.truncate()
+
+        cv2.imshow("img", img)
+        if cv2.waitKey(1) == 27:
+            break
+
+    camera.stop_preview()
